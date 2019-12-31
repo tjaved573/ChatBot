55d92e1939b9c4b3f6cf9d370e43112313f2e48f
Taimoor Javed <tjaved@purdue.edu>
2020-01-01 04:32:26 +0500

    created POST webhook

--- webhook.js
+++ webhook.js
@@ -11,11 +11,28 @@ const server = app.listen(PORT, () => {
     console.log('Address of server is ', server.address());
   });
 
-app.get("/hello", (req, res) => {
-  console.log("Req variable has value ", req);  // The server response functions are stored in the res parameter;
-                        // req is a variable that stores all the information for the incoming request from the client
-  res.send("Hello world");
+app.get('/', (req, res) => {  // The server response functions are stored in the res parameter; How server responds
+                              // req is a variable that stores all the information for the incoming request from the client
+
+                              // res.send("Correct page loaded");
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'thegoldentux'){
+    res.status(200).send(req.query['hub.challenge']); // 200 - OK
+  }else {
+    res.status(403).end();  // 403 - Forbidden
+  }
 });
 
+app.post('/', (req, res) => {
+  console.log(req.body);
+  if (req.body.object === 'page'){
+    req.body.entry.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.message && event.message.text) {
+          sendMessage(event);
+        }
+      });
+    });
+    res.status(200).end();
+  }
 
-
+});
